c07e6018d7b8ed9a62ea2652f461eb6ca20fabd3
nathandidier <ndidier3@yahoo.com>
2023-10-12 10:15:56 -0500

    font size

--- App/SDM/User_Interface/Utils/get_font_size.py
+++ App/SDM/User_Interface/Utils/get_font_size.py
@@ -2,8 +2,8 @@ from sys import platform
 
 def get_font_size(type):
   if platform == "linux" or platform == "linux2":
-    return 16 if type == 'header' else 13
+    return 16 if type == 'header' else 12
   elif platform == "darwin":
-    return 16 if type == 'header' else 13
+    return 16 if type == 'header' else 12
   elif platform == "win32":
-    return 14 if type == 'header' else 11
+    return 13 if type == 'header' else 10
