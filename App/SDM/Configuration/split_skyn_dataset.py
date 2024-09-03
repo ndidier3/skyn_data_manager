4493b4a37325473f3d30b6abfea3197d9b9c061c
nathandidier <ndidier3@yahoo.com>
2024-09-02 22:44:47 -0400

    file splitting by email

--- App/SDM/Configuration/split_skyn_dataset.py
+++ App/SDM/Configuration/split_skyn_dataset.py
@@ -2,14 +2,9 @@ import pandas as pd
 from datetime import datetime, timedelta
 from SDM.Configuration.configuration import configure_timestamp_column
 
-def split_skyn_dataset(data_to_split, split_time, interval, exclude_start = None, exclude_end = None):
+def split_skyn_dataset(data_to_split, split_time):
   data_to_split['datetime'] = configure_timestamp_column(data_to_split)
   data_to_split.sort_values(by='datetime', inplace=True)
-
-  exclude_timeframe = exclude_start != 'None' and exclude_end != 'None'
-  # if exclude_timeframe:
-  #   exclude_start_hour, exclude_start_minute = map(int, exclude_start.split(':'))
-  #   exclude_end_hour, exclude_end_minute = map(int, exclude_end.split(':'))
 
   datasets = {}
   for unique_day in data_to_split['datetime'].dt.date.unique().tolist():
@@ -17,24 +12,34 @@ def split_skyn_dataset(data_to_split, split_time, interval, exclude_start = None
     start = datetime(unique_day.year, unique_day.month, unique_day.day, hour, minute)
     end = start + timedelta(hours=24)
     data = data_to_split[(data_to_split['datetime'] > start) & (data_to_split['datetime'] < end)]
-    if exclude_timeframe:
-      start_time = pd.to_datetime(exclude_start + ':00').time()
-      end_time = pd.to_datetime(exclude_end + ':00').time()
-      # for day_idx in [0, 1]:
-      #   if day_idx == 0:
-      #     exclude_start_time = datetime(unique_day.year, unique_day.month, unique_day.day, exclude_start_hour, exclude_start_minute)
-      #     exclude_end_time = exclude_start_time + timedelta(hours=exclude_interval_hours)
-      #   elif day_idx == 1:
-      #     exclude_start_time = exclude_start_time + timedelta(hours=24)
-      #     exclude_end_time = exclude_end_time + timedelta(hours=24)
-      data = data[~((data['datetime'].dt.time > start_time) & (data['datetime'].dt.time <= end_time))]
-    
     if len(data) > 0:
       datasets[str(min(data['datetime'])) + ' - ' + str(max(data['datetime']))] = data
 
   return datasets
 
+def split_skyn_dataset_by_email(data_to_split, split_time):
+    data_to_split['datetime'] = configure_timestamp_column(data_to_split)
+    data_to_split.sort_values(by='datetime', inplace=True)
 
+    all_datasets = {}
+
+    email_groups = data_to_split.groupby('email')
+
+    for email, group in email_groups:
+        datasets = {}
+
+        for unique_day in group['datetime'].dt.date.unique().tolist():
+            hour, minute = map(int, split_time.split(':'))
+            start = datetime(unique_day.year, unique_day.month, unique_day.day, hour, minute)
+            end = start + timedelta(hours=24)
+            data = group[(group['datetime'] > start) & (group['datetime'] < end)]
+            if len(data) > 0:
+                key = f"{email}: {min(data['datetime'])} - {max(data['datetime'])}"
+                datasets[key] = data
+
+        all_datasets[email] = datasets
+
+    return all_datasets
 
 
 
