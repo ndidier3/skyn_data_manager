3e33278397eacbd402a9fc85b27e4d76bcfaed15
Nathan Didier <ndidier3@yahoo.com>
2024-09-30 12:40:56 -0400

    dataset id detection

--- App/SDM/Scripts/run_many.py
+++ App/SDM/Scripts/run_many.py
@@ -1,4 +1,10 @@
 from SDM.Skyn_Processors.skyn_dataset import skynDataset
+from SDM.User_Interface.Utils.filename_tools import extract_dataset_identifier
+from tkinter import *
+from tkinter import filedialog
+from tkinter.filedialog import askopenfile
+from tkinter import messagebox
+
 from datetime import date
 import pandas as pd
 from pathlib import Path
@@ -27,15 +33,14 @@ if not os.path.exists(analyses_out):
   os.mkdir(analyses_out)
 
 #153 left out
-subids = [101, 102, 106, 112, 113, 114, 115, 117, 118, 120, 121, 122, 123, 127, 130, 
-          132, 133, 134, 138, 139, 140, 141, 143, 146, 147, 149, 150, 151, 155, 157, 
-          159, 160, 161, 162, 165, 167, 171, 172, 174, 180, 181, 183, 185, 186, 189, 
-          190, 194, 198, 199, 202, 204, 206, 207, 208, 209, 210, 211, 212, 213, 214, 
-          215, 216, 218, 219, 220, 221, 222, 223, 227, 233, 236, 237, 238, 241, 243,
-          246, 247, 250, 251, 253, 255, 256, 258, 259, 260, 267, 270, 271, 272, 273,
-          274, 276, 277, 280, 281, 282, 286, 291, 292, 295, 296]
-
-subids = [153]
+# subids = [101, 102, 106, 112, 113, 114, 115, 117, 118, 120, 121, 122, 123, 127, 130, 
+#           132, 133, 134, 138, 139, 140, 141, 143, 146, 147, 149, 150, 151, 153, 155, 
+#           157, 159, 160, 161, 162, 165, 167, 171, 172, 174, 180, 181, 183, 185, 186, 
+#           189, 190, 194, 198, 199, 202, 204, 206, 207, 208, 209, 210, 211, 212, 213, 
+#           214, 215, 216, 218, 219, 220, 221, 222, 223, 227, 233, 236, 237, 238, 241, 
+#           243, 246, 247, 250, 251, 253, 255, 256, 258, 259, 260, 267, 270, 271, 272, 
+#           273, 274, 276, 277, 280, 281, 282, 286, 291, 292, 295, 296]
+subids = [5003]
 
 datasets = []
 no_file_subids = []
@@ -55,8 +60,18 @@ for subid in subids:
     file_path = None
     no_file_subids.append(subid)
     print(f"File for subid {subid} not found.")
-  
-  if file_path: 
+
+  if not file_path:
+    messagebox.showerror('Error', 'Invalid file type.')
+  else:
+    # <subid>_<datasetID>_<text>.xlsx/csv
+    # datasetID is a 3-digit integer
+    # First character = burstID
+    # Second-Third character = dayNumber
+    dataset_identifier = extract_dataset_identifier(file_path)
+    if dataset_identifier == '':
+      messagebox.showerror('Error', 'Dataset ID is not found in filename.')
+    
     sdm_processor = skynDataset(
       str(file_path),
       data_out,
